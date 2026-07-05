5ae350fae915e463decfa53659806149970c2977
GioCastiglioni <g.castiglioni@ug.uchile.cl>
2026-07-04 22:20:12 -0400

    Fixed ASL with original implementation

--- getsits/utils/losses.py
+++ getsits/utils/losses.py
@@ -740,45 +740,53 @@ class FocalLossSoftMax(nn.Module):
         return 'FocalLoss'
 
 
-class AsymmetricLoss(nn.Module):
-    def __init__(self, gamma_neg=4.0, gamma_pos=1.0, clip=0.05, eps=1e-8):
-        """
-        Args:
-            gamma_neg (float): Focusing parameter for negative samples.
-            gamma_pos (float): Focusing parameter for positive samples.
-            clip (float): Probability margin for hard clipping of easy negatives.
-            eps (float): Small value to avoid log(0).
-        """
-        super(AsymmetricLoss, self).__init__()
+class AsymmetricLossOptimized(nn.Module):
+
+    def __init__(self, gamma_neg=4, gamma_pos=1, clip=0.05, eps=1e-8, disable_torch_grad_focal_loss=False):
+        super(AsymmetricLossOptimized, self).__init__()
+
         self.gamma_neg = gamma_neg
         self.gamma_pos = gamma_pos
         self.clip = clip
+        self.disable_torch_grad_focal_loss = disable_torch_grad_focal_loss
         self.eps = eps
 
+        # prevent memory allocation and gpu uploading every iteration, and encourages inplace operations
+        self.targets = self.anti_targets = self.xs_pos = self.xs_neg = self.asymmetric_w = self.loss = None
+
     def forward(self, x, y):
+        """"
+        Parameters
+        ----------
+        x: input logits
+        y: targets (multi-label binarized vector)
         """
-        Args:
-            x (torch.Tensor): Logits of shape (B, C).
-            y (torch.Tensor): Binary targets of shape (B, C).
-        """
-        # Calculate probabilities
-        x_sigmoid = torch.sigmoid(x)
-        xs_pos = x_sigmoid
-        xs_neg = 1.0 - x_sigmoid
+
+        self.targets = y
+        self.anti_targets = 1 - y
+
+        # Calculating Probabilities
+        self.xs_pos = torch.sigmoid(x)
+        self.xs_neg = 1.0 - self.xs_pos
 
         # Asymmetric Clipping
-        if self.clip > 0:
-            xs_neg = (xs_neg + self.clip).clamp(max=1)
+        if self.clip is not None and self.clip > 0:
+            self.xs_neg.add_(self.clip).clamp_(max=1)
 
-        # Log probability clamping
-        los_pos = y * torch.log(xs_pos.clamp(min=self.eps))
-        los_neg = (1 - y) * torch.log(xs_neg.clamp(min=self.eps))
+        # Basic CE calculation
+        self.loss = self.targets * torch.log(self.xs_pos.clamp(min=self.eps))
+        self.loss.add_(self.anti_targets * torch.log(self.xs_neg.clamp(min=self.eps)))
 
         # Asymmetric Focusing
-        loss_pos = los_pos * (1 - xs_pos) ** self.gamma_pos
-        loss_neg = los_neg * (xs_neg) ** self.gamma_neg
+        if self.gamma_neg > 0 or self.gamma_pos > 0:
+            if self.disable_torch_grad_focal_loss:
+                torch.set_grad_enabled(False)
+            self.xs_pos = self.xs_pos * self.targets
+            self.xs_neg = self.xs_neg * self.anti_targets
+            self.asymmetric_w = torch.pow(1 - self.xs_pos - self.xs_neg,
+                                          self.gamma_pos * self.targets + self.gamma_neg * self.anti_targets)
+            if self.disable_torch_grad_focal_loss:
+                torch.set_grad_enabled(True)
+            self.loss *= self.asymmetric_w
 
-        loss = -(loss_pos + loss_neg)
-        
-        # Mean across classes and batch
-        return loss.mean()
+        return -self.loss.sum()
